f13c17740a2c289c7bf3a04640a5f802acf47ef0
Harsh Raj <harshraj9142@gmail.com>
2025-11-27 17:25:01 +0530

    Add rate limiting middleware and configure Upstash dependencies

---
+++ backend/config/upstash.js
@@ -0,0 +1,14 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import "dotenv/config";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),   // <-- correct
+  limiter: Ratelimit.slidingWindow(4, "60 s"),
+});
+
+export default ratelimit;
+
+
+
+
---
+++ backend/middleware/rateLimiter.js
@@ -0,0 +1,20 @@
+import  ratelimit  from "../config/upstash.js";
+
+
+const rateLimiter = async (req, res, next) => {
+    try {
+        const { success } = await ratelimit.limit(`rate-limit-${req.ip}`);
+
+        if (!success) {
+            return res.status(429).json({ message: "Too many requests. Please try again later." });
+        }
+
+        next();
+    } catch (error) {
+        console.error("Rate limiting error:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export default rateLimiter;
+
